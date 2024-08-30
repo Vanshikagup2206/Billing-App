755d9f276e790a1f0e7aef6a9c528fd6a0664426
Vanshika Gupta <vanshikagupta0009@gmail.com>
2024-08-30 12:57:47 +0530

    commit

--- app/src/main/java/com/vanshika/billingapp/DataAdapterClass.kt
+++ app/src/main/java/com/vanshika/billingapp/DataAdapterClass.kt
@@ -1,7 +1,5 @@
 package com.vanshika.billingapp
 
-import androidx.core.location.LocationRequestCompat.Quality
-
 data class DataAdapterClass(
     var name : String ?= "",
     var quality: Int ?= 0
